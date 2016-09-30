badaa6eaebaf504e5c44d7244d8ab83b46ecb56f
Kevin Johnston <kjohnston1149@gmail.com>
2016-09-30 12:17:57 -0700

    Add text and css formatting to first pane in html code

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@ $(document).foundation();
 $('.dropdown').foundation();
 
 //UI logic
-// $(document).ready(function() {
-//   $("button").click(function(event) {
-//     event.preventDefault();
-//     $("#menu-dropdown").toggle();
-//   });
-//
-//
-// });
+$(document).ready(function() {
+
+});
